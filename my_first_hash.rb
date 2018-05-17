ffbaa22604f284d1ef6f67787b52a26a94d9587c
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 05:52:17 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,13 +7,17 @@ def shipping_manifest
   the_manifest = {'whale bone corsets' => 5, 'porcelain vases' => 2, 'oil paintings' => 3}
 end
 
+
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest['oil paintings']
+                 
+
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
 end
 
 def adding
@@ -23,13 +27,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest['muskets'] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest['gun powder'] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
